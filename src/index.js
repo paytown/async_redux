2e6138e9c552e7f889b805c532c6d639a12b8071
Payton Lower <paytown@me.com>
2019-05-29 14:20:04 -0700

    middleware built per ryan example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
+
+console.log('outside');
+
+const logger = store => next => action => {
+  console.log('Before reducer', store.getState());
+  next(action);
+  console.log('After reducer', store.getState());
+};
+
+function reducer(state = 'poo', action) {
+  switch(action.type) {
+    case 'MIDDLEWARE':
+      return 'howdy';
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
 );
+
+store.dispatch({
+  type: 'MIDDLEWARE'
+});
