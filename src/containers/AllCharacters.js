c391d75def70721eee9e3fbac64f5ca9cf5ffa43
Payton Lower <paytown@me.com>
2019-05-30 10:24:22 -0700

    all characters container

---
+++ src/containers/AllCharacters.js
@@ -0,0 +1,47 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+  getCharacters,
+  getCharactersLoading,
+  getCharactersErr
+} from '../selectors/characterSelectors';
+import Characters from '../components/characters/Characters';
+import { fetchCharacters } from '../actions/fetchCharactersAction';
+
+class AllCharacters extends PureComponent{
+  static propTypes = {
+    fetch: PropTypes.func.isRequired,
+    characters: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
+    err: PropTypes.object
+  }
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    const { characters, loading, err } = this.props;
+    if(loading) return <h1>Loading...</h1>;
+    if(err) return <h1>Error...</h1>;
+    return <Characters character={characters} />;
+  }
+}
+
+const mapStateToProps = state => ({
+  characters: getCharacters(state),
+  loading: getCharactersLoading(state),
+  err: getCharactersErr(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    dispatch(fetchCharacters());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AllCharacters);
