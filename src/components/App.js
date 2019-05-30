8c8f0e5c502cb9a5ed46c7ce8afc3dd0d3f6b5b7
Payton Lower <paytown@me.com>
2019-05-30 14:24:37 -0700

    basic router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import AllCharacters from '../containers/AllCharacters';
 
 export default function App() {
   return (
-    <>
-      <AllCharacters />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={AllCharacters} />
+      </Switch>
+    </Router>
   );
 }
