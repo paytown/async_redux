36d93ac75b645733b17948ded23affa92e9d1051
Payton Lower <paytown@me.com>
2019-05-30 09:26:27 -0700

    hello world

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Character from './characters/Character';
+
+export default function App() {
+  return (
+    <>
+    <h1>Hello world</h1>
+      {/* <Character /> */}
+    </>
+  );
+}
