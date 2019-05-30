3de6d8af782627e1e47d52d8e5f535c1c7aa8eac
Payton Lower <paytown@me.com>
2019-05-30 14:14:51 -0700

    refactored to use promise

--- src/actions/fetchCharactersAction.js
+++ src/actions/fetchCharactersAction.js
@@ -4,22 +4,29 @@ export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
 export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
 export const FETCH_CHARACTERS_ERR = 'FETCH_CHARACTERS_ERR';
 
-export const fetchCharacters = () => dispatch => {
-  dispatch({
-    type: FETCH_CHARACTERS_LOADING
-  });
+export const fetchCharacters = () => ({
+  type: FETCH_CHARACTERS,
+  pendingType: FETCH_CHARACTERS_LOADING,
+  error: FETCH_CHARACTERS_ERR,
+  payload: fetchApi()
+});
 
-  return fetchApi()
-    .then(character => {
-      dispatch({
-        type: FETCH_CHARACTERS,
-        payload: character
-      });
-    })
-    .catch(err => {
-      dispatch({
-        type: FETCH_CHARACTERS_ERR,
-        payload: err
-      });
-    });
-};
+// export const fetchCharacters = () => dispatch => {
+//   dispatch({
+//     type: FETCH_CHARACTERS_LOADING
+//   });
+
+//   return fetchApi()
+//     .then(character => {
+//       dispatch({
+//         type: FETCH_CHARACTERS,
+//         payload: character
+//       });
+//     })
+//     .catch(err => {
+//       dispatch({
+//         type: FETCH_CHARACTERS_ERR,
+//         payload: err
+//       });
+//     });
+// };
